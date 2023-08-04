9cb3885637939deba9c40b4ee292bd6771326c53
yoke <44898730+yoke0104x@users.noreply.github.com>
2023-08-04 08:55:55 +0800

    Update juejin.py

--- juejin.py
+++ juejin.py
@@ -26,34 +26,35 @@ def _request(cookie, data, SENDKEY):
         if draw.json()["err_no"] == 0:
             data = draw.json()["data"];
             msg = "掘金签到成功！免费一次抽奖结果为 【" + data["lottery_name"] + "】";
-            try:
-                # 获取围观列表接口
-                watcherList = requests.post("https://api.juejin.cn/growth_api/v1/lottery_history/global_big", headers={"cookie": cookie}, params=data,data={
-                    "page_no":1,
-                    "page_size":5
-                });
-                history = watcherList.json()["data"]["lotteries"][0];
-                print("围观用户日志:"+history)
-                # 沾喜气接口
-                joyful = requests.post("https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky", headers={"cookie": cookie}, params=data,data={
-                    "history_id":history["history_id"]
-                });
-                res = joyful.json();
-                print("沾喜气日志:"+res)
-                if res["err_no"] == 0:
-                    if not res["data"]["has_dip"]:
-                        user_name = history["user_name"]
-                        msg += f"你沾了一下 {user_name} 的喜气，幸运值增加了 10 点，下次抽奖说不定能抽中";
-                    else:
-                        msg += "今天你已经沾过喜气，明天再来吧！据说坚持不懈的姿势，持续访问稀土掘金，抽中大奖几率更高噢～";
-                else:
-                    msg += res["err_msg"];
-            except Exception as e:
-               msg += "沾喜气失败！";
         else:
             msg = draw.json()["err_msg"];
     else:
         msg = "掘金签到失败！原因：" + check_in.json()["err_msg"];
+
+    try:
+        # 获取围观列表接口
+        watcherList = requests.post("https://api.juejin.cn/growth_api/v1/lottery_history/global_big", headers={"cookie": cookie}, params=data,data={
+            "page_no":1,
+            "page_size":5
+        });
+        history = watcherList.json()["data"]["lotteries"][0];
+        print("围观用户日志:"+history)
+        # 沾喜气接口
+        joyful = requests.post("https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky", headers={"cookie": cookie}, params=data,data={
+            "history_id":history["history_id"]
+        });
+        res = joyful.json();
+        print("沾喜气日志:"+res)
+        if res["err_no"] == 0:
+            if not res["data"]["has_dip"]:
+                user_name = history["user_name"]
+                msg += f"你沾了一下 {user_name} 的喜气，幸运值增加了 10 点，下次抽奖说不定能抽中";
+            else:
+                msg += "今天你已经沾过喜气，明天再来吧！据说坚持不懈的姿势，持续访问稀土掘金，抽中大奖几率更高噢～";
+        else:
+            msg += res["err_msg"];
+    except Exception as e:
+       msg += "沾喜气失败！";
 
     requests.get("https://sctapi.ftqq.com/" + SENDKEY + ".send", params={"title": "python掘金签到", "desp": msg});
     
