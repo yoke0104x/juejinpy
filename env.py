491f6e361dcc7caec65785583fedd9d08fedb703
yoke <m17633465271@163.com>
2023-08-04 18:33:55 +0800

    Update: juejin.py

---
+++ env.py
@@ -0,0 +1,14 @@
+# Author: yoke
+# Date: 2020/10/28
+# Desc: 环境变量
+
+# Cookie
+JUEJIN_COOKIE = "";
+
+# Server酱
+JUEJIN_SENDKEY = "";
+
+# 数据
+JUEJIN_DATA = "";
+
+# 如需使用环境变量，请取消juejin.py的注释
--- juejin.py
+++ juejin.py
@@ -4,6 +4,14 @@ import time
 import schedule
 import os
 
+# 引入 env.py 文件
+# from env import JUEJIN_COOKIE, JUEJIN_SENDKEY, JUEJIN_DATA;
+# # 设置环境变量
+# os.environ["JUEJIN_COOKIE"] = JUEJIN_COOKIE;
+# os.environ["JUEJIN_SENDKEY"] = JUEJIN_SENDKEY;
+# os.environ["JUEJIN_DATA"] = JUEJIN_DATA;
+
+
 def JueJin():
     cookie = os.environ["JUEJIN_COOKIE"];
     SENDKEY = os.environ["JUEJIN_SENDKEY"];
@@ -29,32 +37,31 @@ def _request(cookie, data, SENDKEY):
         else:
             msg = draw.json()["err_msg"];
     else:
-        msg = "掘金签到失败！原因：" + check_in.json()["err_msg"];
+        msg = "掘金签到失败！原因：\n\n" + check_in.json()["err_msg"] + "\n\n";
 
-    try:
-        # 获取围观列表接口
-        watcherList = requests.post("https://api.juejin.cn/growth_api/v1/lottery_history/global_big", headers={"cookie": cookie}, params=data,data={
-            "page_no":1,
-            "page_size":5
-        });
-        history = watcherList.json()["data"]["lotteries"][0];
-        print("围观用户日志:"+history)
-        # 沾喜气接口
-        joyful = requests.post("https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky", headers={"cookie": cookie}, params=data,data={
-            "history_id":history["history_id"]
-        });
-        res = joyful.json();
-        print("沾喜气日志:"+res)
-        if res["err_no"] == 0:
-            if not res["data"]["has_dip"]:
-                user_name = history["user_name"]
-                msg += f"你沾了一下 {user_name} 的喜气，幸运值增加了 10 点，下次抽奖说不定能抽中";
-            else:
-                msg += "今天你已经沾过喜气，明天再来吧！据说坚持不懈的姿势，持续访问稀土掘金，抽中大奖几率更高噢～";
+    # 获取围观列表接口
+    watcherList = requests.post("https://api.juejin.cn/growth_api/v1/lottery_history/global_big", headers={"cookie": cookie}, params=data,data={
+        "page_no":1,
+        "page_size":5
+    });
+    history = watcherList.json()["data"]["lotteries"][0];
+    print("围观用户日志:"+ str(history));
+    # 沾喜气接口
+    joyful = requests.post("https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky", headers={"cookie": cookie}, params=data,data={
+        "history_id":history["history_id"]
+    });
+    res = joyful.json();
+    print("沾喜气日志:"+str(res))
+    if res["err_no"] == 0:
+        if not res["data"]["has_dip"]:
+            user_name = history["user_name"]
+            msg += f"你沾了一下 `{user_name}` 的喜气，幸运值增加了 10 点，下次抽奖说不定能抽中";
         else:
-            msg += res["err_msg"];
-    except Exception as e:
-       msg += "沾喜气失败！";
+            msg += "今天你已经沾过喜气，明天再来吧！据说坚持不懈的姿势，持续访问稀土掘金，抽中大奖几率更高噢～";
+    else:
+        msg += res["err_msg"];
+
+    msg += "\n\n来个图片\n\n![screenshot](https://api.yoke0104.com/api/dou)"
 
     requests.get("https://sctapi.ftqq.com/" + SENDKEY + ".send", params={"title": "python掘金签到", "desp": msg});
     
@@ -63,8 +70,7 @@ def _request(cookie, data, SENDKEY):
 if __name__ == "__main__":
     print("掘金签到： " + JueJin());
     # 每天8点执行一次
-    # schedule.every().day.at("08:00").do(WuChunTaoJueJin);
-    # schedule.every().day.at("08:00").do(DmJueJin);
+    # schedule.every().day.at("08:00").do(Juejin);
     # while True:
     #     schedule.run_pending();
     #     time.sleep(1);
